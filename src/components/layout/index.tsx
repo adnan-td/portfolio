0f4db06716b655b5c571cc8bb2b20122d73a253f
Adnan Shabbir Husain <generaladnan139@gmail.com>
2023-11-06 21:33:15 +0530

    update: re-added loader

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,14 +2,41 @@
 
 import { NavStatusProvider } from "@/context/navstatus/navstatus.context";
 import { WidthContext } from "@/context/screenwidth/screenwidth.context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { FollowerProvider } from "react-mouse-follower";
+import loaderjson from "@/json/loader.json";
+import Lottie from "react-lottie";
 
 export default function Sublayout({ children }: { children: React.ReactNode }) {
   const { screenwidth } = useContext(WidthContext);
   return (
-    <FollowerProvider visible={screenwidth > 640}>
-      <NavStatusProvider>{children}</NavStatusProvider>
-    </FollowerProvider>
+    <Loader>
+      <FollowerProvider visible={screenwidth > 640}>
+        <NavStatusProvider>{children}</NavStatusProvider>
+      </FollowerProvider>
+    </Loader>
+  );
+}
+
+function Loader({ children }: { children: React.ReactNode }) {
+  const [isLoading, setLoadingPage] = useState<boolean>(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadingPage(false);
+    }, 1250);
+  }, []);
+  return (
+    <>
+      {isLoading && (
+        <div className="absolute top-0 left-0 w-screen h-screen flex justify-center items-center z-[1000] bg-white">
+          <Lottie
+            options={{ loop: true, autoplay: true, animationData: loaderjson }}
+            height={200}
+            width={200}
+          />
+        </div>
+      )}
+      <div className={isLoading ? "hidden" : ""}>{children}</div>
+    </>
   );
 }
